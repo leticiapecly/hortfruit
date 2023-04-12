1759b5ac615ff52862151c5dec13d22efefa341c
Letícia Pecly <leticiappecly19@gmail.com>
2023-04-12 17:47:34 -0300

    hortfruit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import Alface from "./Assets/alface.png"
+import Beterraba from "./Assets/beterraba.png"
+import Cenoura from "./Assets/cenoura.png"
+import Cereja from "./Assets/cereja.png"
+import Laranja from "./Assets/laranja.png"
+import Limao from "./Assets/limao.png"
+import Manga from "./Assets/manga.png"
+import Tomate from "./Assets/tomate.png"
+
+function App() {
+  const [frutas,setFrutas] = useState([
+    Alface, Beterraba, Cenoura, Cereja, Laranja, Limao, Manga, Tomate
+  ])
+
+  const [numeros,setNumeros] = useState(0)
+
+
+  return (
+    <main>
+      <div>
+      {frutas.map((item)=>(
+        <img src={item} alt=""/>
+      ))}
+      </div>
+    </main>
+  );
+}
+
+export default App;
